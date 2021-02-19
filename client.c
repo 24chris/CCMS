996a3f9cc1899c0632a0d271a22375ce955dcafb
24chris <40150356+24chris@users.noreply.github.com>
2021-02-19 04:07:34 +0300

    Update client.c

--- client.c
+++ client.c
@@ -1,114 +1,191 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <string.h>
-#define MAX_PATIENTS 100
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-FILE *sick_file;
+#define PORT 8080
 
+int clientSocket, ret;
+struct sockaddr_in serverAddr;
+char buffer[1024];
 
-
-//socket globals
-int sockfd, n;
-struct sockaddr_in serv_addr;
-struct hostent *server;
-char buffer[256];
-socklen_t portno = 4000;
-char Name_of_district[30];
-
-
-
-
-void display_commands(){
-
-    char choice[5][30] = {"1:ADD_YOUR_PATIENT", "2:ADD_MULTIPLE_CASES","3:CHECK_CURRENT_FILE_STATUS", "4:SEARCH_CRITERIA"};
-    int next_choice;
-    puts("-------Lists of Choices-----\t");
-    for (next_choice = 0; next_choice < 5; next_choice++)
-    {
-        puts(choice[next_choice]);
-    }
-};
-char *get_command(int selected_choice)
-// call functions
+void connection()
 {
-    if (selected_choice == 1){
-        return "1";
-    }
-    else if (selected_choice == 2){
-        return "2";
-    }
-    else if (selected_choice == 3){
-        printf("\n");
-        return "3";
-    }
-    else if (selected_choice == 4){
-        return "4";
-    } else{
-      return "5";
-    }
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket < 0)
+	{
+		printf("oops there was an error .\n");
+		exit(1);
+	}
+	printf("client socket created successfully.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+	if (ret < 0)
+	{
+		printf("Error in connection please try again.\n");
+		exit(1);
+	}
+	printf("serverConnected.\n");
 }
-int main(int argc, char *argv[]){
-    int Choose;
-    if (argc != 2){
-        printf("\nUsage:%s <ip server>\n",argv[2]);
-    }
+void usage_commands(){
 
-      sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	printf("\n\tHOW TO USE THE SYSTEM\n");
+	printf("\nCommands are specified as below for the betterment of the system use the commands as instructed\n");
+	printf("\nAddPatient command> usage Addpatient patient_name tabkey date(y-m-d) gender\n");
+	printf("\nAddpatient filename.txt>usage Addpatient specific the file name(only text based files)\n");
+	printf("\nCheck_status >usage Check_status\n");
+	printf("\nSearch command>usage Search enter what to search enter by date(y-m-d) or name\n");
+	printf("\n");
+   
+	
+}
+int check_command(char *command){
+	 int com1 = strncmp(command, "Addpatient", 10);
+	 int com2 = strncmp(command, "Check_status", 12);
+	 int com3 = strncmp(command, "Search",6);
+	 int com4 = strncmp(command, "Addpatientlist", 14);
+	
 
-    server = gethostbyname("127.0.0.1");
-
-    if (server == NULL)
-    {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
-    }
-
-    bzero((char *)&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(4000);
-    //connect to server with server address which is set above (serv_addr)
-
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("ERROR while connecting");
-        exit(1);
-    }
-
-    puts("-------------------------Welcome to the covid-19 case management and reporting system-------------------");
-    puts("\n");
-
-
-    printf("Enter name of district for example Kampala: ");
-
-    scanf("%s", Name_of_district);
-
-      while (1){
-          display_commands();
-
-         printf("\nSelectCommand:");
-        scanf("%d", &Choose);
-       snprintf(buffer,sizeof(buffer),"%s",get_command(Choose));
-
-        n = write(sockfd, buffer, 256);
-
-        if (n < 0){
-            perror("ERROR while writing to socket");
-            exit(1);
-        }
-
-        bzero(buffer, 256);
-        int rnd = read(sockfd, buffer, 256);
-
-        if (rnd < 0) {
-            perror("ERROR while reading from socket");
-            exit(1);
-        }
-        printf("\n\nServer replied: %s \n\n", buffer);
-
-
-    }
+	 if(com1==0|| com2==0 || com3==0 || com4==0){
+		 return 1;
+	 }
+	else{
+		return 0;
+	}
 }
 
+void main()
+{   
+	printf("\n\tOUR COVID 19  SOCKET SYSTEM\n");
+	printf("\n");
+		char District[50];
+	printf("\nEnterDistrict:");
+	fgets(District, 50, stdin);
+	char officer_user_name[50];
+	printf("\nEnter the officer_user_name:");
+	//scanf("%s", officer_user_name);
+	fgets(officer_user_name, 50, stdin);
+	usage_commands();
+	while (1)
+	{	
+		char command[50];
+		char name[30];
+		char date[30];
+
+	back_to_top:
+	    printf("\nOfficerUserName:%-10s \nDistrict\t:%-20s\n", officer_user_name, District);
+		printf("\n----------------------------------------\n");
+		bzero(command, sizeof(command));
+		bzero(name, sizeof(name));
+		bzero(date, sizeof(date));
+		printf("ExecuteCommand:");
+		fgets(buffer, 1024, stdin);
+		if(!check_command(buffer)){
+			printf("\n\nWrong command entered please try again\n\n");
+ 			goto back_to_top;
+
+		}
+
+		buffer[strlen(buffer) - 1] = '\0';
+
+		sscanf(buffer, "%s %s %s", command, name, date);
+		//Addpatient katende nicolas 2021-02 M
+		//Addpatient bruno.txt
+		printf("%s, %s, %s \n", command, name, date);
+	
+		//strncmp() checker
+
+		if ((strlen(date) == 0) && (!strncmp(buffer ,"Addpatient",10)))
+		{    
+			char *txtExt, *check_txt;
+			//chat tx[];
+		//	Addpatient bruno txt//.
+
+			txtExt = strtok(buffer, ".");
+			while (txtExt != NULL)
+			{
+				check_txt = txtExt;
+				txtExt = strtok(NULL, ".");
+			}
+
+			if (strcmp(check_txt, "txt") == 0)
+			{
+				FILE *filePtr = fopen(name, "r+");
+				if (filePtr == NULL)
+				{
+					printf("file read error!\n");
+				}
+				else
+				{
+			
+					while (fgets(buffer, 100, filePtr) != NULL)
+					{
+						buffer[strlen(buffer)-1]='\0';
+						//puts(store);
+						connection();
+						send(clientSocket, buffer, strlen(buffer), 0);
+						bzero(buffer, sizeof(buffer));
+					}
+				}
+
+				goto back_to_top;
+			}
+			else
+			{
+				printf("Wrong file format\n");
+				goto back_to_top;
+			}
+		}
+		else if((strlen(date) !=0) && (!strncmp(buffer, "Addpatient", 10))){
+	
+		
+		//concat
+		strcat(buffer , " ");
+		strcat(buffer, officer_user_name);
+		buffer[strlen(buffer)-1]='\0';
+		connection();
+		send(clientSocket, buffer, strlen(buffer), 0);
+		
+
+
+		}
+		else{
+	
+		connection();
+		send(clientSocket, buffer, strlen(buffer), 0);
+
+		}
+
+
+
+		if (strcmp(buffer, ":exit") == 0)
+		{
+			close(clientSocket);
+			printf("Server has been disconnected.\n");
+			exit(1);
+		}
+
+		if (recv(clientSocket, buffer, 1024, 0) < 0)
+		{
+			printf("Error in receiving info.\n");
+		}
+		else
+		{ 	
+			printf("\n\nserver:%s\n\n", buffer);
+		}
+		goto back_to_top;
+		//close(clientSocket);
+	}
+
+	return;
+}
